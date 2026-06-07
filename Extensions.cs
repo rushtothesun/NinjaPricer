277367442264f0ccd4173ed7218da1e133e0bbcb
instant.sc <code@insc.cc>
2026-06-07 21:03:51 +0200

    Handle formatting edge cases

--- Extensions.cs
+++ Extensions.cs
@@ -6,10 +6,7 @@ public static class Extensions
 {
     public static string FormatNumber(this double number, int significantDigits, double maxInvertValue = 0, bool forceDecimals = false)
     {
-        if (double.IsNaN(number))
-        {
-            return "NaN";
-        }
+        if (TryFormatEdgeCase(number, out var formatNumber)) return formatNumber;
 
         if (number == 0)
         {
@@ -23,9 +20,46 @@ public static class Extensions
 
         if (Math.Abs(number) < maxInvertValue)
         {
-            return $"1/{Math.Round((decimal)(1 / number), 1):#.#}";
+            var inverted = 1 / number;
+            if (TryFormatEdgeCase(number, out var formatInverted)) return $"1/{formatInverted}";
+
+            if (Math.Abs(inverted) > (double)decimal.MaxValue)
+            {
+                return $"1/{inverted:0.#e+0}";
+            }
+
+            return $"1/{Math.Round((decimal)inverted, 1):#.#}";
+        }
+
+        if (Math.Abs(number) > (double)decimal.MaxValue)
+        {
+            return number.ToString("0.##e+0");
         }
 
         return Math.Round((decimal)number, significantDigits).ToString($"#,##0.{new string(forceDecimals ? '0' : '#', significantDigits)}");
+    }
+
+    private static bool TryFormatEdgeCase(double number, out string formatNumber)
+    {
+        if (double.IsNaN(number))
+        {
+            formatNumber = "NaN";
+            return true;
+        }
+
+        if (double.IsPositiveInfinity(number))
+        {
+            formatNumber = "inf";
+            return true;
+        }
+
+        if (double.IsNegativeInfinity(number))
+        {
+            formatNumber = "-inf";
+            return true;
+        }
+
+        formatNumber = null;
+        return false;
     }
 }
